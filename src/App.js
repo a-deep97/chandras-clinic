69fa329bdbc0f1921fcf38ddce0c445f6b98ceb3
a-deep97 <a.deep97@outlook.com>
2024-01-28 23:32:13 +0530

    First commit for adding basic pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './components/utilities/home';
+import Book from './components/utilities/book';
+import Contact from './components/utilities/contact';
+import About from './components/utilities/about';
+import Auth from './components/utilities/auth';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path= '/auth' element={<Auth/>}></Route>
+          <Route path= '/' element={<Home/>}></Route>
+          <Route path= '/home' element={<Home/>}></Route>
+          <Route path= '/book' element={<Book/>}></Route>
+          <Route path= '/contact' element={<Contact/>}></Route>
+          <Route path= '/about' element={<About/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
